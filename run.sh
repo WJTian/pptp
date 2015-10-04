66ab2b1b4766f49738afd978c6a60b08aa25fb1e
WJTian <root@i-uyau3jsk.(none)>
2015-10-05 03:21:41 +0800

    bugfix

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-iptables -t nat -A POSTROUTING -s 192.168.0.0/24 -j MASQUERADE
-iptables save
+iptables -t nat -A POSTROUTING -s 192.168.0.0/24 -o eth0  -j MASQUERADE
+iptables-save > /etc/sysconfig/iptables
 echo 1 > /proc/sys/net/ipv4/ip_forward
-wget https://github.com/WJTian/pptp/archive/master.zip && unzip master && unzip master && cd pptp-master\
+wget -o master https://github.com/WJTian/pptp/archive/master.zip && unzip -f master.zip && cd pptp-master\
 && rpm -ivh ppp* && rpm -ivh pptp* && pptpd
 
